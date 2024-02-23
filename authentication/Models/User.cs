d19acb07a71e4941c282067eca5cdff24a79ab0f
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-02-23 19:01:31 +0530

    models updates

--- authentication/Models/User.cs
+++ authentication/Models/User.cs
@@ -8,8 +8,8 @@ namespace authentication.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; } 
-        public required string Email { get; set; }
-        public required string Username { get; set; }
-        public required string Password { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
