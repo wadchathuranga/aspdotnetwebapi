d8213c77df58746bdc8878f31787f19db1d35d7e
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-02-23 09:48:00 +0530

    crud controllers added

--- authentication/Controllers/UserController.cs
+++ authentication/Controllers/UserController.cs
@@ -17,16 +17,44 @@ namespace authentication.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<List<User>>> Register(UserInputDTO userDTO)
+        public async Task<ActionResult<List<User?>>> Register(UserInputDTO userDTO)
         {
-            var response = await _userService.CreateUserAsync(userDTO);
+            var response = await _userService.CreateUser(userDTO);
             return Ok(response);
         }
 
         [HttpPost("login")]
         public async Task<ActionResult> Login(LoginInputDTO loginDTO)
         {
-            var response = await _userService.LoginAsync(loginDTO);
+            var response = await _userService.Login(loginDTO);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User?>> GetAllUsers()
+        {
+            var response = await _userService.GetAllUsers();
+            return Ok(response);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<User?>> UpdateUser(int id, UserUpdateDTO userUpdateDTO)
+        {
+            var response = await _userService.UpdateUser(id, userUpdateDTO);
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User?>> GetSingleUserById(int id)
+        {
+            var response = await _userService.GetSingleUserById(id);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User?>> DeleteUserById(int id)
+        {
+            var response = await _userService.DeleteUserById(id);
             return Ok(response);
         }
     }
