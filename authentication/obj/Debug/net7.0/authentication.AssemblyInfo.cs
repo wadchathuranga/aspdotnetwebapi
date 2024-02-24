6eddad09b053bbae769a475c6da045a2c3ef9b34
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-02-24 11:16:55 +0530

    new migration created

--- authentication/obj/Debug/net7.0/authentication.AssemblyInfo.cs
+++ authentication/obj/Debug/net7.0/authentication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("authentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d815027c2d44172dadd7195be5e66898c0796c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a143c312a4ef094a9e5c42a0953716a94384b29b")]
 [assembly: System.Reflection.AssemblyProductAttribute("authentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("authentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
