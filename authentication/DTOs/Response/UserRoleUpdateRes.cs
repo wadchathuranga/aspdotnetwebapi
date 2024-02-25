e8b430cf49ff443767c37fa76c2819d84226b17e
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-02-25 12:57:16 +0530

    role update dto added

---
+++ authentication/DTOs/Response/UserRoleUpdateRes.cs
@@ -0,0 +1,11 @@
+﻿namespace authentication.DTOs.Response
+{
+    public class UserRoleUpdateRes
+    {
+        public string? Message { get; set; }
+
+        public bool Result { get; set; }
+
+        public List<string>? Errors { get; set; }
+    }
+}
