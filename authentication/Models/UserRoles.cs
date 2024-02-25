c508a21b206abe66a38305b765b6256766cd3d23
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-02-25 09:19:43 +0530

    static roles define

---
+++ authentication/Models/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace authentication.Models
+{
+    public static class UserRoles
+    {
+        public const string OWNER = "OWNER";
+        public const string ADMIN = "ADMIN";
+        public const string USER = "USER";
+    }
+}
